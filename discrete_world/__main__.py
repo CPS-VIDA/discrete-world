c8e88f66cde3db4a4abd1280ee3ca9b7bcea89d3
Anand Balakrishnan <anandbala1597@gmail.com>
2020-12-15 19:01:35 -0800

    Make sure numpy < 1.19.4

--- discrete_world/__main__.py
+++ discrete_world/__main__.py
@@ -1,31 +1,33 @@
 # Python defaults
 import os
-import sys
-import argparse
 import pickle
-import random
+import argparse
+from typing import List, Tuple
+
+import discrete_world.plotting as P
 
 # Custom imports
-import design_obstacles as DO
-import plotting as P
-from agent import Agent
-from grid import GridWorld as G
+import discrete_world.design_obstacles as DO
+from discrete_world.grid import GridWorld as G
+from discrete_world.agent import Agent
+
 
 def readWorld(fp):
     """ Reads a grid-world from an .env file. """
-    with open(fp, 'rb') as data_file:
+    with open(fp, "rb") as data_file:
         data = pickle.load(data_file)
     return data
+
 
 def main(env_name, create_obstacles=False):
     # Define the grid-world start and goal locations
     rows = 5
     cols = 5
-    init_pos = (rows-1, 0)
-    goals = [(0, cols-1), (rows-1, cols-1)]
-    obstacles = []
+    init_pos = (rows - 1, 0)
+    goals = [(0, cols - 1), (rows - 1, cols - 1)]
+    obstacles = []  # type: List[Tuple[int, int]]
     p_slip = 0.8
-    env_file = os.path.join(env_dir, env_name+".env")
+    env_file = os.path.join(env_dir, env_name + ".env")
     g = G(rows, cols, init_pos, goals, obstacles, p_slip)
 
     # Design the obstacles and save it in the env_file
@@ -43,27 +45,29 @@ def main(env_name, create_obstacles=False):
     # ----- This is where you define your RL agent. I just chose a random policy ----
     robot.gen_policy()
 
-    # Show the plots of the grid-world and its reward 
+    # Show the plots of the grid-world and its reward
     # Image available in the fig_dir under parent.
     P.gen_plots(robot, fig_dir, env_name)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # All environment files are saved in the 'env' folder
     env_dir = os.path.join(os.pardir, "envs")
     if not os.path.exists(env_dir):
         os.makedirs(env_dir)
-    
+
     # All figures are saved in 'figs' folder and then go figure
     fig_dir = os.path.join(os.pardir, "figs")
     if not os.path.exists(fig_dir):
         os.makedirs(fig_dir)
 
-    parser = argparse.ArgumentParser(
-        description='Grid-World setup')
-    parser.add_argument('f', type=str, help='filename of environment; automatically appends .env extension: Example: env1')
-    parser.add_argument('--d',
-                        action='store_true',
-                        help='design obstacles in PyGame')
+    parser = argparse.ArgumentParser(description="Grid-World setup")
+    parser.add_argument(
+        "f",
+        type=str,
+        help="filename of environment; automatically appends .env extension: Example: env1",
+    )
+    parser.add_argument("--d", action="store_true", help="design obstacles in PyGame")
     args = parser.parse_args()
 
     env_name = args.f
